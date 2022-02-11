abfa6e22555649e1d269268bd65dd86767238bc8
Sophia Barrett <sophia@yoursummit.media>
2022-02-11 15:54:14 -0700

    fix duplicate products when adding to cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -7,14 +7,28 @@ import { pluralize } from "../../utils/helpers";
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
-  const addToCart = () => {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...item, purchaseQuantity: 1 },
-    });
-  };
+  const { cart } = state;
 
   const { image, name, _id, price, quantity } = item;
+
+  const addToCart = () => {
+    // find cart item with matching id
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+    // if there was a match, call UPDATE with a new puchase quantity
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 },
+      });
+    }
+  };
 
   return (
     <div className="card px-1 py-1">
